6b5a18f362cc6472766d8f03802ca4a257225952
Peter Nixon <listuser@peternixon.net>
2010-03-19 17:45:32 +0200

    Add Domain to Admin

--- dyndns/admin.py
+++ dyndns/admin.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from dyndns.models import Record
+from dyndns.models import Domain,Record
 
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'content')
-    list_filter = ['type']
+    list_display = ('name', 'type', 'content',)
+    list_filter = ['type', 'change_date',]
     search_fields  = ('name','content',)
 
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type',)
+    list_filter = ['type', 'last_check',]
+    search_fields  = ('name','type',)
+
+admin.site.register(Domain,DomainAdmin)
 admin.site.register(Record,RecordAdmin)
