f66d248720e1eae18bbb358c40fa0c0b0393b80c
Peter Nixon <listuser@peternixon.net>
2010-04-30 19:56:26 +0300

    fix the DB model so that it allows nulls in the same way as the default PowerDNS schema

--- dyndns/models.py
+++ dyndns/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Domain(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(unique=True, max_length=255)
-    master = models.CharField(max_length=128)
-    last_check = models.IntegerField()
-    type = models.CharField(max_length=6)
-    notified_serial = models.IntegerField()
-    account = models.CharField(max_length=40)
+    master = models.CharField(max_length=128, blank=True, null=True)
+    last_check = models.IntegerField(blank=True, null=True)
+    type = models.CharField(max_length=6, blank=True, null=True)
+    notified_serial = models.IntegerField(blank=True, null=True)
+    account = models.CharField(max_length=40, blank=True, null=True)
     def __unicode__(self):
         return self.name
     class Meta:
@@ -17,12 +17,12 @@ class Domain(models.Model):
 class Record(models.Model):
     id = models.IntegerField(primary_key=True)
     domain = models.ForeignKey(Domain)
-    name = models.CharField(max_length=255)
-    type = models.CharField(max_length=6)
-    content = models.CharField(max_length=255)
-    ttl = models.IntegerField()
-    prio = models.IntegerField()
-    change_date = models.IntegerField()
+    name = models.CharField(max_length=255, blank=True, null=True)
+    type = models.CharField(max_length=6, blank=True, null=True)
+    content = models.CharField(max_length=255, blank=True, null=True)
+    ttl = models.IntegerField(blank=True, null=True)
+    prio = models.IntegerField(blank=True, null=True)
+    change_date = models.IntegerField(blank=True, null=True)
     def __unicode__(self):
         return self.name
     class Meta:
@@ -31,6 +31,6 @@ class Record(models.Model):
 class Supermaster(models.Model):
     ip = models.CharField(max_length=25)
     nameserver = models.CharField(max_length=255)
-    account = models.CharField(max_length=40)
+    account = models.CharField(max_length=40, blank=True, null=True)
     class Meta:
         db_table = u'supermasters'
