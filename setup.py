1ff51949c059c90c3b36209822d419dee31a512b
Peter Nixon <listuser@peternixon.net>
2010-05-23 15:29:05 +0300

    Cleanup setup.py a bit more

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -8,19 +10,23 @@ except ImportError:
     ez_setup.use_setuptools()
     from setuptools import setup, find_packages
 
-import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
-    name = "django-powerdns",
-    version = "0.2",
+    name = 'django-powerdns',
+    version = '0.2',
     url = 'http://bitbucket.org/peternixon/django-powerdns/',
     download_url = 'http://bitbucket.org/peternixon/django-powerdns/downloads/',
     license = 'BSD',
-    description = "PowerDNS administration module for Django",
+    description = 'PowerDNS administration module for Django',
+    long_description=read('README.rst'),
     author = 'Peter Nixon',
     author_email = 'listuser@peternixon.net',
     packages = find_packages(),
     include_package_data = True,
+    platforms='any',
     classifiers = [
         'Development Status :: 4 - Beta',
         'Framework :: Django',
@@ -33,18 +39,8 @@ setup(
         'Topic :: Internet :: Name Service (DNS)',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
     install_requires=[
         'Django>=1.2',
-    ]
-    long_description = """\
-Django PowerDNS
-===============
-
-Welcome to the PowerDNS module for Django.
-
-This module allows easy administration of PowerDNS records stored in an SQL database by leveraging
-the standard Django Admin module. You may also use the Django PowerDNS module as part of a larger
-project to programatically modify your DNS records.
-"""
+    ],
 )
