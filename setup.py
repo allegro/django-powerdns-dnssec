27108bad751ee840fe569a0ae290caca4485b8d1
Peter Nixon <listuser@peternixon.net>
2010-05-23 14:39:34 +0300

    Modify setup to better describe the package before first release

--- setup.py
+++ setup.py
@@ -7,28 +7,31 @@ except ImportError:
     import ez_setup
     ez_setup.use_setuptools()
     from setuptools import setup, find_packages
-    
+
 import os
 
 setup(
     name = "django-powerdns",
     version = "0.1",
     url = 'http://bitbucket.org/peternixon/django-powerdns/',
-	download_url = 'http://bitbucket.org/peternixon/django-dyndns/downloads/',
+    download_url = 'http://bitbucket.org/peternixon/django-powerdns/downloads/',
     license = 'BSD',
     description = "PowerDNS administration module for Django",
     author = 'Peter Nixon',
     author_email = 'listuser@peternixon.net',
     packages = find_packages(),
     include_package_data = True,
-    zip_safe = False,
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
