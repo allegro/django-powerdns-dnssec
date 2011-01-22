4d61d2682c71327d45aa59423285e800d9508f37
Peter Nixon <listuser@peternixon.net>
2011-01-23 01:41:10 +0200

    Expose Domainmetadata and Cryptokey models in the Admin UI

--- powerdns/admin.py
+++ powerdns/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from powerdns.models import Domain, Record, Supermaster
+from powerdns.models import Cryptokey, Domain, Domainmetadata, Record, Supermaster
 
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'content', 'ttl', 'prio', 'change_date',)
@@ -19,6 +19,18 @@ class SupermasterAdmin(admin.ModelAdmin):
     list_filter = ['ip', 'account',]
     search_fields  = ('ip', 'nameserver',)
 
+class DomainmetadataAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'kind', 'content',)
+    list_filter = ['domain', 'kind',]
+    search_fields  = ('content',)
+
+class CryptokeyAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'flags', 'active', 'content',)
+    list_filter = ['domain', 'active',]
+    search_fields  = ('content',)
+
 admin.site.register(Domain,DomainAdmin)
 admin.site.register(Record,RecordAdmin)
 admin.site.register(Supermaster,SupermasterAdmin)
+admin.site.register(Domainmetadata,DomainmetadataAdmin)
+admin.site.register(Cryptokey,CryptokeyAdmin)
