bb130d4f1c19ede073118916871e048374edc3fe
Łukasz Langa <lukasz@langa.pl>
2012-11-03 18:33:05 +0100

    bumped version to 0.9.0, updated setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,23 @@ except ImportError:
     ez_setup.use_setuptools()
     from setuptools import setup, find_packages
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name = 'django-powerdns',
-    version = '0.2',
+    version = '0.9.0',
     url = 'http://bitbucket.org/peternixon/django-powerdns/',
     download_url = 'http://bitbucket.org/peternixon/django-powerdns/downloads/',
     license = 'BSD',
     description = 'PowerDNS administration module for Django',
     long_description=read('README.rst'),
-    author = 'Peter Nixon',
+    author = 'Peter Nixon, Łukasz Langa',
     author_email = 'listuser@peternixon.net',
     packages = find_packages(),
     include_package_data = True,
@@ -32,11 +36,16 @@ setup(
         'Framework :: Django',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
         'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
         'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
