d0988c4baca55d57aa66871b0bb813a02ce0874a
Łukasz Langa <lukasz@langa.pl>
2012-11-06 01:15:35 +0100

    further admin improvements

--- powerdns/admin.py
+++ powerdns/admin.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from django.forms import NullBooleanSelect
 from django.contrib import admin
+from django.contrib.admin.widgets import AdminRadioSelect
+from django.db import models
 from powerdns.models import (CryptoKey, Domain, DomainMetadata, Record,
                              SuperMaster)
+
+
+class NullBooleanRadioSelect(NullBooleanSelect, AdminRadioSelect):
+    pass
 
 
 class RecordAdmin(admin.ModelAdmin):
     list_display = (
         'name', 'type', 'content', 'domain', 'ttl', 'prio', 'change_date',
     )
-    list_filter = ('type', 'ttl', 'domain',)
+    list_filter = ('type', 'ttl', 'auth', 'domain',)
+    list_per_page = 250
+    save_on_top = True
     search_fields = ('name', 'content',)
+    #radio_fields = {'auth': admin.HORIZONTAL}
     readonly_fields = ('change_date', 'ordername',)
     fieldsets = (
         (None, {
@@ -21,6 +31,13 @@ class RecordAdmin(admin.ModelAdmin):
             'fields': ('prio', 'ttl', 'ordername', 'change_date',)
         }),
     )
+    formfield_overrides = {
+        models.NullBooleanField: {
+            'widget': NullBooleanRadioSelect(
+                attrs={'class': 'radiolist inline'}
+            ),
+        },
+    }
 
 
 class DomainMetadataInline(admin.TabularInline):
@@ -32,7 +49,10 @@ class DomainAdmin(admin.ModelAdmin):
     inlines = [DomainMetadataInline]
     list_display = ('name', 'type', 'last_check', 'account',)
     list_filter = ('type', 'last_check', 'account',)
+    list_per_page = 250
+    save_on_top = True
     search_fields = ('name',)
+    radio_fields = {'type': admin.HORIZONTAL}
     readonly_fields = ('notified_serial',)
 
 
@@ -45,12 +65,16 @@ class SuperMasterAdmin(admin.ModelAdmin):
 class DomainMetadataAdmin(admin.ModelAdmin):
     list_display = ('domain', 'kind', 'content',)
     list_filter = ('kind', 'domain',)
+    list_per_page = 250
+    save_on_top = True
     search_fields = ('content',)
 
 
 class CryptoKeyAdmin(admin.ModelAdmin):
     list_display = ('domain', 'flags', 'active', 'content',)
     list_filter = ('active', 'domain',)
+    list_per_page = 250
+    save_on_top = True
     search_fields = ('content',)
 
 
