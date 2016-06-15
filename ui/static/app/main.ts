693215b16e0f0bcaacc9f31ff071c0ab9b8b342a
Piotr Jarolewski <piotr.jarolewski@allegrogroup.com>
2016-06-15 12:44:39 +0200

    Add sentry to Angular App

--- ui/static/app/main.ts
+++ ui/static/app/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, provide } from "@angular/core";
+import { enableProdMode, provide, ExceptionHandler } from "@angular/core";
 import { bootstrap } from "@angular/platform-browser-dynamic";
 import { HTTP_PROVIDERS } from "@angular/http";
 import { ROUTER_PROVIDERS } from "@angular/router-deprecated";
@@ -9,6 +9,7 @@ import { AuthService } from "./auth/auth.service";
 import { ConfigService } from "./config.service";
 import { HttpClient } from "./http-client";
 import { LocalStorage } from "./local-storage";
+import { CustomExceptionHandler } from "./exception-handler";
 
 // get java script debug variable from ui/templates/ui/index.html
 declare var debug: any;
@@ -20,6 +21,7 @@ if (!debug) {
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
   HTTP_PROVIDERS,
+  provide(ExceptionHandler, {useClass: CustomExceptionHandler}),
   provide(LocationStrategy, {useClass: HashLocationStrategy}),
   provide(LocalStorage, {useClass: LocalStorage}),
   provide(AuthService, {useClass: AuthService}),
