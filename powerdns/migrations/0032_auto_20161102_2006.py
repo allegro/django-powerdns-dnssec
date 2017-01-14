373f520dc68ca22a3c2c681637143bb26c317598
Drew Hubl <drew.hubl@gmail.com>
2017-01-13 19:31:01 -0700

    Fix 'DateTimeField received a naive datetime' warnings during tests caused by migrations

--- powerdns/migrations/0032_auto_20161102_2006.py
+++ powerdns/migrations/0032_auto_20161102_2006.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.utils import timezone
 import datetime
 
 
@@ -15,37 +16,37 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='deleterequest',
             name='created',
-            field=models.DateTimeField(auto_now_add=True, verbose_name='date created', default=datetime.datetime(1970, 1, 1, 0, 0)),
+            field=models.DateTimeField(auto_now_add=True, verbose_name='date created', default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='deleterequest',
             name='modified',
-            field=models.DateTimeField(auto_now=True, verbose_name='last modified', default=datetime.datetime(1970, 1, 1, 0, 0)),
+            field=models.DateTimeField(auto_now=True, verbose_name='last modified', default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='domainrequest',
             name='created',
-            field=models.DateTimeField(auto_now_add=True, verbose_name='date created', default=datetime.datetime(1970, 1, 1, 0, 0)),
+            field=models.DateTimeField(auto_now_add=True, verbose_name='date created', default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='domainrequest',
             name='modified',
-            field=models.DateTimeField(auto_now=True, verbose_name='last modified', default=datetime.datetime(1970, 1, 1, 0, 0)),
+            field=models.DateTimeField(auto_now=True, verbose_name='last modified', default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='recordrequest',
             name='created',
-            field=models.DateTimeField(auto_now_add=True, verbose_name='date created', default=datetime.datetime(1970, 1, 1, 0, 0)),
+            field=models.DateTimeField(auto_now_add=True, verbose_name='date created', default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='recordrequest',
             name='modified',
-            field=models.DateTimeField(auto_now=True, verbose_name='last modified', default=datetime.datetime(1970, 1, 1, 0, 0)),
+            field=models.DateTimeField(auto_now=True, verbose_name='last modified', default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)),
             preserve_default=False,
         ),
     ]
